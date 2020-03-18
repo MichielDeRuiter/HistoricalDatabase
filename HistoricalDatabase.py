a40de6cee761e3adb8634a884b0d61198385262d
https://github.com/jorijnsmit/dsp.c4 <mideruit@gmail.com>
2020-03-18 21:05:27 +0100

    Delete implemented

--- HistoricalDatabase.py
+++ HistoricalDatabase.py
@@ -21,39 +21,39 @@ for change in change_stream:
     print(dumps(change))
     print('')
 
-    operation_type = change["operationType"]
+    operation_type = change['operationType']
 
     if operation_type == 'insert':
         dic = {}
-        id = change["fullDocument"]["_id"]
-        for k, v in change["fullDocument"].items():
+        id = change['fullDocument']['_id']
+        for k, v in change['fullDocument'].items():
             dic[k] = [{'timestamp': str(datetime.now()), 'value': v}]
         dic['_id'] = id
         history_collection.insert_one(dic)
 
     elif operation_type == 'update':
     # update
-        # for k, v in change["updateDescription"]["updatedFields"].items():
+        # for k, v in change['updateDescription']['updatedFields'].items():
         #     print(k, v)
         #
-        # print(change["updateDescription"]["updatedFields"])
-        # print(change["documentKey"])
-        # print(change["documentKey"]["_id"])
-        # print(history_collection.find_one({"_id": change["documentKey"]["_id"]}))
-        dic = history_collection.find_one({"_id": change["documentKey"]["_id"]})
+        # print(change['updateDescription']['updatedFields'])
+        # print(change['documentKey'])
+        # print(change['documentKey']['_id'])
+        # print(history_collection.find_one({'_id': change['documentKey']['_id']}))
+        dic = history_collection.find_one({'_id': change['documentKey']['_id']})
 
-        print(history_collection.find_one({"_id": change["documentKey"]["_id"]}).items())
-        print(change["updateDescription"]["updatedFields"].items())
+        print(history_collection.find_one({'_id': change['documentKey']['_id']}).items())
+        print(change['updateDescription']['updatedFields'].items())
 
-        for k, v in history_collection.find_one({"_id": change["documentKey"]["_id"]}).items():
+        for k, v in history_collection.find_one({'_id': change['documentKey']['_id']}).items():
             if type(v) != bson.objectid.ObjectId:
-                for kk, vv in change["updateDescription"]["updatedFields"].items():
+                for kk, vv in change['updateDescription']['updatedFields'].items():
                     if k == kk:
                         dic[kk].append({'timestamp': str(datetime.now()), 'value': vv})
                     # else:
                         # print(kk, vv)
                         # dic[kk] = [{'timestamp': str(datetime.now()), 'value': vv}]
-                for kk in change["updateDescription"]["removedFields"]:
+                for kk in change['updateDescription']['removedFields']:
                     if k == kk:
                         dic[k].append({'timestamp': str(datetime.now()), 'deleted': True})
                     # else:
@@ -62,31 +62,35 @@ for change in change_stream:
         print(dic)
 
         history_collection.find_one_and_update(
-            {"_id": change["documentKey"]["_id"]},
-            {"$set": dic}, upsert=True)
+            {'_id': change['documentKey']['_id']},
+            {'$set': dic}, upsert=True)
 
     # TODO:
     # elif operation_type == 'replace':
-    # elif operation_type == 'delete':
-
-
+    #     print('replace')
+    elif operation_type == 'delete':
+        dic = history_collection.find_one({'_id': change['documentKey']['_id']})
+        dic['deleted_timestamp'] = datetime.now()
+        history_collection.find_one_and_update(
+            {'_id': change['documentKey']['_id']},
+            {'$set': dic}, upsert=True)
 
 
     # history_collection.find_one_and_update(
-    #     {"_id": history_collection.find_one({"_id": change["documentKey"]["_id"]})},
-    #     {"$set":
-    #          {"age": 32}
+    #     {'_id': history_collection.find_one({'_id': change['documentKey']['_id']})},
+    #     {'$set':
+    #          {'age': 32}
     #      }, upsert=True
     # )
 
-    # history_collection.insert_one(change["fullDocument"])
+    # history_collection.insert_one(change['fullDocument'])
 
     # history_collection.find_one_and_update(
-    #     {"_id": ObjectId(change["fullDocument"]["_id"])},
-    #     {"$set":
-    #          {"some field": "OBJECTROCKET ROCKS3!!"}
+    #     {'_id': ObjectId(change['fullDocument']['_id'])},
+    #     {'$set':
+    #          {'some field': 'OBJECTROCKET ROCKS3!!'}
     #      }, upsert=True
     # )
-    # history_collection.insert_one(change["fullDocument"])
+    # history_collection.insert_one(change['fullDocument'])
 
-# client.test.test.insert_one({"hello": "world"}).inserted_id
+# client.test.test.insert_one({'hello': 'world'}).inserted_id
