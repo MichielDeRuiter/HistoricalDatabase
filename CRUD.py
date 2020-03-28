0ea3fa110cd8277e5a3871dd4ced63ffdc7050d7
https://github.com/jorijnsmit/dsp.c4 <mideruit@gmail.com>
2020-03-28 02:44:52 +0100

    Delete object working.

--- CRUD.py
+++ CRUD.py
@@ -8,9 +8,9 @@ col = client.test.test
 # print(col.insert_one({'name': 'Foo', 'age': 1337}).inserted_id)
 
 # update document
-doc = col.find_one_and_update(
-    {'_id': ObjectId('5e7bc3944edccf2c7ac9b846')},
-    {'$set': {'name': 'Bar', 'age': 1340}}, upsert=True)
+# doc = col.find_one_and_update(
+#     {'_id': ObjectId('5e7bc3944edccf2c7ac9b846')},
+#     {'$set': {'name': 'Bar', 'age': 1340}}, upsert=True)
 
 # delete field
 # doc = col.find_one_and_update(
@@ -18,4 +18,4 @@ doc = col.find_one_and_update(
 #     {'$unset': {'age': ''}}, upsert=True)
 
 # delete document
-# col.delete_one({'_id': ObjectId('5e727e25d7cf7cd79a956561')})
+col.delete_one({'_id': ObjectId('5e7eaae1e988123fd75b46ae')})
--- Restore.py
+++ Restore.py
@@ -19,22 +19,25 @@ def restore_object(object_id, timestamp):
     del historical_object['_id']
     restored_object = {}
 
-    for k, v in historical_object.items():
+    if 'deleted_timestamp' in historical_object:
+        print('object {} deleted'.format(object_id))
+    else:
+        for k, v in historical_object.items():
 
-        # if the last field in the array is deleted, delete it, otherwise include it
-        if 'deleted' in v[-1]:
-            pass
-        else:
-            ts_list = []
-            for ts in v:
-                ts_list.append(ts['timestamp'])
-            ts_list = [datetime.fromisoformat(ts) for ts in ts_list]
-            restored_object[k] = next((item for item in v if item['timestamp'] == str(nearest(ts_list, timestamp))), None)['value']
+            # if the last field in the array is deleted, delete it, otherwise include it
+            if 'deleted' in v[-1]:
+                pass
+            else:
+                ts_list = []
+                for ts in v:
+                    ts_list.append(ts['timestamp'])
+                ts_list = [datetime.fromisoformat(ts) for ts in ts_list]
+                restored_object[k] = next((item for item in v if item['timestamp'] == str(nearest(ts_list, timestamp))), None)['value']
 
-    restored_object['_id'] = id
-    return restored_object
+        restored_object['_id'] = id
+        return restored_object
 
 
 # TODO: Reconstruct collection to timestamp
 if __name__ == '__main__':
-    print(restore_object('5e7bc3944edccf2c7ac9b846', '2020-03-25 21:48:30.962634'))
+    print(restore_object('5e7eaae1e988123fd75b46ae', '2020-03-25 21:48:30.962634'))
